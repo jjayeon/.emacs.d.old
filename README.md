bdc029333a787f8dfbeab37a3f0ad31d6d3c2066
jjayeon <autumn.chiu.136@gmail.com>
2021-06-14 23:31:58 -0400

    starting a new .emacs.d

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-this is mostly so that i can clone my customization stuff between computers
-you can look at it if you want but it's not very interesting
+# my-emacs
+My emacs config. one file because I keep my config very light.
+
+To use, you have to install use-package from MELPA, but after that this should handle the rest automatically.  See [here](https://jwiegley.github.io/use-package/installation/) for more info.
+
+This will create a bunch of files in ~/.emacs.d/ if you're on Linux.  If you're on another OS, you're on your own.
+
--- init.el
+++ init.el
@@ -1,30 +1,81 @@
-;; (global-set-key (kbd "<right>") 'right-char)
-;; (global-set-key (kbd "<left>") 'left-char)
-;; (global-set-key (kbd "<up>") 'previous-line)
-;; (global-set-key (kbd "<down>") 'next-line)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
-(load "~/.emacs.d/no-externals.el")
-(load "~/.emacs.d/load-packages.el")
+(eval-when-compile
+  (require 'use-package))
 
-(global-unset-key (kbd "C-c C-c"))
-(global-set-key (kbd "C-c C-c") 'comment-region)
+(use-package paredit
+  :ensure t
+  :init
+  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook           #'enable-paredit-mode))
 
-(setq-default indent-tabs-mode nil)
+(use-package paredit-everywhere
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook 'paredit-everywhere-mode))
 
-(global-set-key (kbd "C-x <up>") 'windmove-up)
-(global-set-key (kbd "C-x <down>") 'windmove-down)
-(global-set-key (kbd "C-x <left>") 'windmove-left)
-(global-set-key (kbd "C-x <right>") 'windmove-right)
-(put 'upcase-region 'disabled nil)
+(use-package js2-mode
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  ;; (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+  )
+
+(use-package rjsx-mode
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.jsx?$" . rjsx-mode)) ;; auto-enable for .js/.jsx files
+  (setq sgml-basic-offset 4))
+
+(use-package prettier-js
+  :ensure t
+  :init
+  (add-hook 'js-mode-hook 'prettier-js-mode)
+  (add-hook 'js2-mode-hook 'prettier-js-mode)
+  (setq prettier-js-args '(
+			   "--tab-width" "4"
+			   )))
+
+(use-package csharp-mode
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.cs$" . csharp-mode)))
+  
+(use-package flycheck
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook #'global-flycheck-mode))
+
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+	 ("\\.md\\'" . markdown-mode)
+	 ("\\.markdown\\'" . markdown-mode)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (csharp-mode use-package prettier flycheck js2-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
